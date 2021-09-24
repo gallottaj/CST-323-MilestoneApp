f172fab651c2675756f3c36aec52707c7127f563
Joey <gallo@DESKTOP-BO1GFI1>
2021-09-23 17:55:09 -0700

    used faker to add 100 bogus products and descriptions

--- Display Data in a Table/Controllers/ProductController.cs
+++ Display Data in a Table/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Display_Data_in_a_Table.Models;
+﻿using Bogus;
+using Display_Data_in_a_Table.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,11 @@ namespace Display_Data_in_a_Table.Controllers
             productsList.Add(new ProductModel { Id = 2, Name = "Webcam", Price = 45.99m, Description = "Enables you to attend more Zoom meetings." });
             productsList.Add(new ProductModel { Id = 3, Name = "USB hard drive", Price = 56.99m, Description = "Use to back up all of your work" });
             productsList.Add(new ProductModel { Id = 4, Name = "Wireless mouse", Price = 23.98m, Description = "An external mouse" });
+
+            for (int i = 0; i < 100; i++)
+            {
+                productsList.Add(new Faker<ProductModel>().RuleFor(p => p.Id, i + 5).RuleFor(p => p.Name, f => f.Commerce.ProductName()).RuleFor(p => p.Price, f => f.Random.Decimal(100)).RuleFor(p => p.Description, f => f.Rant.Review()));
+            }
 
             return View(productsList);
         }
