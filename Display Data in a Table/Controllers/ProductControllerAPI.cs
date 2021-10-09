b1f9a5e2899ce0e2263a6520756802ba90af43df
Joey <gallo@DESKTOP-BO1GFI1>
2021-10-09 12:02:37 -0700

    added product api controller

---
+++ Display Data in a Table/Controllers/ProductControllerAPI.cs
@@ -0,0 +1,64 @@
+﻿using Bogus;
+using Display_Data_in_a_Table.Models;
+using Display_Data_in_a_Table.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Display_Data_in_a_Table.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class ProductControllerAPI : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductsDAO products = new ProductsDAO();
+
+            return View(products.GetAllProducts());
+        }
+
+        public IActionResult SearchResults(string searchTerm)
+        {
+            ProductsDAO products = new ProductsDAO();
+
+            List<ProductModel> productList = products.SearchProducts(searchTerm);
+            return View("index", productList);
+        }
+
+        public IActionResult ShowDetails(int id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel foundProduct = products.GetProductByID(id);
+            return View(foundProduct);
+        }
+        public IActionResult Edit(int id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel foundProduct = products.GetProductByID(id);
+            return View("ShowEdit", foundProduct);
+        }
+
+        public IActionResult ProcessEdit(ProductModel product)
+        {
+            ProductsDAO products = new ProductsDAO();
+            products.Update(product);
+            return View("Index", products.GetAllProducts());
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel product = products.GetProductByID(Id);
+            products.Delete(product);
+            return View("Index", products.GetAllProducts());
+        }
+
+        public IActionResult SearchForm()
+        {
+            return View();
+        }
+    }
+}
